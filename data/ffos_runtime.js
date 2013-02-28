00b708c065e942df34132957161210ab2ebfe8ca
Kevin Grandon <kevingrandon@yahoo.com>
2013-02-27 20:16:46 -0800

    Remove old system xhr proxy

--- data/ffos_runtime.js
+++ data/ffos_runtime.js
@@ -95,59 +95,6 @@ window.addEventListener('keyup', function(e) {
 	}
 })
 
-/**
- * Proxies mozSystem XMLHttpRequest
- * This is currently a work-around because system XHR does not work to get icons
- */
- /*
-var defaultXHR = unsafeWindow.XMLHttpRequest
-function XHRProxy(config) {
-	console.log('XMLHTTP REQ CONSTRUCTOR')
-	this.responseType = null
-	this.config = config
-
-	this.onreadystatechange = function() {}
-	this.onerror = function() {}
-}
-
-XHRProxy.prototype = {
-	open: function(method, resource) {
-
-		this.resource = resource
-		this.method = method
-
-		// Proxy icons
-		if (/icons\/60/.test(this.resource)) {
-			
-		}
-		
-		unsafeWindow.setTimeout(function() {
-			var xhr = new defaultXHR(this.config)
-			xhr.open(this.method, this.resource, true)
-			if (this.responseType)
-				xhr.responseType = this.responseType
-			xhr.onreadystatechange = this.onreadystatechange
-			xhr.onerror = this.onerror
-			xhr.send(this.sendData)
-		}.bind(this))
-	},
-	send: function(sendData) {
-		this.sendData = sendData
-	}
-}
-
-unsafeWindow.XMLHttpRequest = function(config) {
-
-	console.log('XMLHttpRequest:', config)
-
-	if (config && config.mozSystem) {
-		return new XHRProxy(config)
-	} else {
-		return new defaultXHR(config)
-	}
-}
-*/
-
 FFOS_RUNTIME.getAppWindow(function(win) {
 	/**
 	 * Adds a nextPaintListener 
